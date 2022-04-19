6547876a3a6670215d22a86521fd44badcba61ca
Manh Tien <tiennm@fabbi.io>
2022-04-19 10:32:32 +0700

    rest

--- users/views.py
+++ users/views.py
@@ -14,8 +14,8 @@ class CustomUserCreate(APIView):
         if reg_serializer.is_valid():
             new_user = reg_serializer.save()
             if new_user:
-                return Response(status=status.HTTP_201_CREATED)
-        return Response({"Message": "Đăng ký Users thành công"}, reg_serializer.errors,
+                return Response({"Message": "Đăng ký Users thành công"}, status=status.HTTP_201_CREATED)
+        return Response({"Message": "Đăng ký Users không thành công"}, reg_serializer.errors,
                         status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -27,6 +27,6 @@ class BlacklistTokenView(APIView):
             refresh_token = request.data['refresh_token']
             token = RefreshToken(refresh_token)
             token.blacklist()
-            return Response(status=status.HTTP_200_OK)
+            return Response({"Message": "Đăng xuất thành công"}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
